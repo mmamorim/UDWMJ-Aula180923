f2971b344ab1211ac1b8646b2d4fa76570e95e76
mmamorim <marc.m.amorim@gmail.com>
2023-10-02 20:34:37 -0300

    versao recreio

--- main0210.js
+++ main0210.js
@@ -3,26 +3,45 @@ console.log('passei aqui');
 
 
 function relogio(tempoAlarme) {
-    return new Promise((result,reject) => {
+    return new Promise((result, reject) => {
         let segundos = 0
         let num = (Math.random() * 100) % 9
-        console.log('num',num);
+        console.log('num', num);
         setInterval(() => {
             segundos++
-            if(segundos >= num) {
+            if (segundos >= num) {
                 reject('OPA! DEU PANE!')
             }
-            if(segundos == tempoAlarme) {
+            if (segundos == tempoAlarme) {
                 result('BEEP BEEP BEEP')
             }
-        },1000)    
+        }, 1000)
     })
 }
 
-let p = relogio(5);
-console.log('p',p);
-p.then((resp) => {
-    console.log('resp',resp);
-}).catch((resp) => {
-    console.log('resp',resp);
-})
+//relogio(5).then((resp) => {
+//    console.log('resp', resp);
+//    relogio(3).then((resp) => {
+//        console.log('resp', resp);
+//    }).catch((resp) => {
+//        console.log('resp', resp);
+//    })    
+//}).catch((resp) => {
+//    console.log('resp', resp);
+//})
+
+
+async function brincaComRelogios() {
+    try {
+        let resp = await relogio(5)
+        console.log('resp', resp)    
+        resp = await relogio(5)
+        console.log('resp', resp)    
+        resp = await relogio(5)
+        console.log('resp', resp)    
+    } catch {
+        console.log('erro');
+    }
+}
+
+brincaComRelogios()
